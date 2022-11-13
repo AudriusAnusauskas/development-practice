6510506575100672ccee88abe956b4907e557795
Audrius Ausauskas <aaaudrius@gmail.com>
2022-11-13 20:23:11 +0200

    wf-19 space removed

--- src/loops/task3.js
+++ src/loops/task3.js
@@ -1,9 +1,9 @@
-const course = 'Fundamentals';
+const course = "Fundamentals";
 let countOfA = 0;
 
-for(let i = 0; i < course.length; i++){
-  if(course[i] === 'a'){
-    countOfA ++;
+for (let i = 0; i < course.length; i++) {
+  if (course[i] === "a") {
+    countOfA++;
   }
   console.log(countOfA);
 }
