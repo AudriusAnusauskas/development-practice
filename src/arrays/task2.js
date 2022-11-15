183d8aa08dad864fa1a557e00c6f3ab3bae46682
Audrius Ausauskas <aaaudrius@gmail.com>
2022-11-15 22:37:33 +0200

    wf-25 task2.js

---
+++ src/arrays/task2.js
@@ -0,0 +1,22 @@
+let arr = [];
+
+let str = "devMentor - path to the developer career";
+
+for (let i = 0; i < str.length; i++) {
+  i % 2 === 0 ? arr.push(str[i]) : 0;
+}
+
+console.log(arr);
+
+let store = [];
+let primes = [];
+for (let i = 2; i <= str.length; ++i) {
+  if (!store[i]) {
+    primes.push(str[i]);
+    for (let j = i << 1; j <= str.length; j += i) {
+      store[j] = true;
+    }
+  }
+}
+console.log(primes);
+///googled the algorythm for primes and adapted to my task
