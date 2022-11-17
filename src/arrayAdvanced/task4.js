b1b45bdc002bec7cd5dbe598b5123e7b229f4de7
Audrius Ausauskas <aaaudrius@gmail.com>
2022-11-17 18:23:29 +0200

    wf-26 task4 apply reduce()

--- src/arrayAdvanced/task4.js
+++ src/arrayAdvanced/task4.js
@@ -1,12 +1,12 @@
-let fn = (objArr) => {
-  const count = {};
-  objArr.forEach((el) => {
-    count[el.name] = (count[el.name] || 0) + 1;
-  });
-  console.log(count);
-};
+// let fn = (objArr) => {
+//   const count = {};
+//   objArr.forEach((el) => {
+//     count[el.name] = (count[el.name] || 0) + 1;
+//   });
+//   console.log(count);
+// };
 
-let peopleArr = [
+const peopleArr = [
   { name: "Tomas", age: 15 },
   { name: "Erika", age: 22 },
   { name: "Tomas", age: 17 },
@@ -15,6 +15,19 @@ let peopleArr = [
   { name: "Tomas", age: 19 },
 ];
 
-fn(peopleArr);
+// fn(peopleArr);
 
 // googled the algorythm, adapted to my solution
+
+const fn2 = (objArr) => {
+  return objArr.reduce((accumulator, currentValue) => {
+    const key = currentValue.name;
+    if (!accumulator[key]) {
+      accumulator[key] = 0;
+    }
+    accumulator[key]++;
+    return accumulator;
+  }, {});
+};
+
+console.log(fn2(peopleArr)); // 0
