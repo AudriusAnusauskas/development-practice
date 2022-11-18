f6021430ef0be0934c49d97cfdcea48762632df1
Audrius Ausauskas <aaaudrius@gmail.com>
2022-11-18 11:45:48 +0200

    wf-26 task3.js destructuring in forEach

--- src/arrayAdvanced/task3.js
+++ src/arrayAdvanced/task3.js
@@ -1,14 +1,27 @@
+// const fn = (objArr) => {
+//   const newArr = [];
+//   objArr.forEach((el) => {
+//     if (el.age >= 18) {
+//       newArr.push(el.name);
+//     }
+//   });
+
+//   return newArr;
+// };
+
 const fn = (objArr) => {
   const newArr = [];
-  objArr.forEach((el) => {
-    if (el.age >= 18) {
-      newArr.push(el.name);
+
+  objArr.forEach(({ name, age }) => {
+    if (age >= 18) {
+      newArr.push(name);
     }
   });
+
   return newArr;
 };
 
-let peopleArr = [
+const peopleArr = [
   { name: "Tomas", age: 15 },
   { name: "Erika", age: 22 },
   { name: "Saulius", age: 17 },
